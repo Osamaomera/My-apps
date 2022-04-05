04273fedc7ef580d6dd2c985a2efb8103b6365d5
Osamaomera <92927320+Osamaomera@users.noreply.github.com>
2022-04-05 22:47:14 +0200

    Add files via upload

---
+++ Dentalclinicsystem/src/main/java/Cheque.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author OsamaAyman
+ */
+public class Cheque extends Payment {
+    
+    private String signature;
+    private String to ;
+    private String from ;
+    private String bank_name;
+
+    public Cheque(String signature, String to, String from, String bank_name, float amount) {
+        super(amount);
+        this.signature = signature;
+        this.to = to;
+        this.from = from;
+        this.bank_name = bank_name;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getBank_name() {
+        return bank_name;
+    }
+
+    public void setBank_name(String bank_name) {
+        this.bank_name = bank_name;
+    }
+    
+    
+}
